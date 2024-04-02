d2e4beecbdaf82ac5adda813975bacc7d8df081d
Art P <fn3x@yandex.ru>
2024-04-03 01:50:37 +0400

    chore: cleaned up remaps

--- lua/remap.lua
+++ lua/remap.lua
@@ -7,9 +7,6 @@ end
 -- filetree
 map("n", "<leader>fp", vim.cmd.Ex, "View filetree")
 
--- map jk to escape
-map("i", "jk", "<ESC>")
-
 map("v", "K", ":m '<-2<CR>gv=gv", "Move line up by 1 line and format it")
 map("v", "J", ":m '>+1<CR>gv=gv", "Move line down by 1 line and format it")
 
@@ -21,12 +18,10 @@ map("n", "N", "Nzzzv", "Previous occurence and center")
 
 -- yank and paste
 map("x", "<leader>p", '"_dP', "Replace without yanking")
-map("n", "<leader>d", '"_d', "Delete without yanking") -- e.g <leader>dd deletes the current line without yanking it
+map("n", "<leader>d", '"_d', "Delete without yanking")
 map("n", "<leader>D", '"_D', "Delete until EOL without yanking")
 map("n", "<leader>c", '"_c', "Change without yanking")
 map("n", "<leader>C", '"_C', "Change until EOL without yanking")
-map("", "<leader>y", '"+y', "Yank to clipboard") -- E.g: <leader>yy will yank current line to os clipboard
-map("", "<leader>Y", '"+y$', "Yank until EOL to clipboard")
 map("n", "<leader>p", '"+p', "Paste after cursor from clipboard")
 map("n", "<leader>P", '"+P', "Paste before cursor from clipboard")
 
